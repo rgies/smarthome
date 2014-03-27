6943b82d91d7587bb7cc9f74a863a33de9ba2838
Robert Gies <robert.gies@cocomore.com>
2014-03-27 09:19:52 +0100

    Add meta tags to display in full screen on ios devices

--- View/DefaultLayout.php
+++ View/DefaultLayout.php
@@ -5,6 +5,8 @@
     <title>Smarthome</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=no">
     <link rel="shortcut icon" href="images/favicon.ico" />
     <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
     <link href="css/bootstrap.min.css" rel="stylesheet" />
